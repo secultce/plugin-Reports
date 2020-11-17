fd44b92c3a353919fb4b53a12384403f076292d5
Wylder Marcos Silva Menezes <wylder.marcos@secult.local>
2020-11-17 17:11:55 -0300

    plugin em andamento

---
+++ layout/button-generation-report.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+
+<a class="btn btn-default download btn-report-evaluation-documental"  ng-click="editbox.open('report-evaluation-documental-options', $event)" rel="noopener noreferrer">Imprimir Resultado</a>
+
+<!--Botão antigo-->
+<a class="btn btn-default download"  href="">aqui</a>
+
+
+<!-- Formulário -->
+<edit-box id="report-evaluation-documental-options" position="top" title="" cancel-label="Cancelar" close-on-cancel="true">
+    <form class="form-report-evaluation-documental-options" action="<?=$route?>" method="POST">
+
+        <label for="publishDate">Data publicação</label>
+        <input type="date" name="publishDate" id="publishDate">
+
+        <label for="from">Formato</label>
+        <select name="fileFormat" id="fileFormat">
+            <option value="pdf" selected >PDF</option>
+            <option value="xls">XLS</option>
+            <option value="doc">DOC</option>
+        </select>
+
+        <button class="btn btn-primary download" type="submit">Imprimir Resultado</button>
+    </form>
+</edit-box>
