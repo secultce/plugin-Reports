c6cb60340815ac3d2415e158fa6716cad3ed0381
Wylder Marcos Silva Menezes <wylder.marcos@secult.local>
2020-11-18 16:52:25 -0300

    desenvolvimento de views em andamento

---
+++ data/resultado-preliminar.php
@@ -0,0 +1,7 @@
+<?php
+
+class ResultadoPreliminar{
+    public function generationData($datePublish){
+        
+    }
+}
--- views/button-generation-report.php
+++ views/button-generation-report.php
@@ -1,17 +1,13 @@
-<?php
+
+<a class="btn btn-default download btn-report-evaluation-documental" href="" ng-click="editbox.open('report-evaluation-documental-options', $event)" rel="noopener noreferrer">Imprimir Resultado</a>
 
 
-?>
-
-<a class="btn btn-default download btn-report-evaluation-documental"  ng-click="editbox.open('report-evaluation-documental-options', $event)" rel="noopener noreferrer">Imprimir Resultado</a>
-
-<!--Botão antigo-->
 <a class="btn btn-default download"  href="">aqui</a>
 
 
-<!-- Formulário -->
+
 <edit-box id="report-evaluation-documental-options" position="top" title="" cancel-label="Cancelar" close-on-cancel="true">
-    <form class="form-report-evaluation-documental-options" action="<?=$route?>" method="POST">
+    <form class="form-report-evaluation-documental-options" action="" method="POST">
 
         <label for="publishDate">Data publicação</label>
         <input type="date" name="publishDate" id="publishDate">
@@ -25,4 +21,5 @@
 
         <button class="btn btn-primary download" type="submit">Imprimir Resultado</button>
     </form>
-</edit-box>
+</edit-box> 
+
