f103ef36238712d70d30d40b77def6e2d1a593d4
Wylder Marcos Silva Menezes <wylder.marcos@secult.local>
2020-11-23 14:25:05 -0300

    Em desenvolvimento

--- views/form-report.php
+++ views/form-report.php
@@ -6,33 +6,14 @@
 <div class="container">
 
 <div class="form-sec">
-  <h4>Contact form</h4>
+  <br>
+  <h4>Insira os parâmetros do relatório selecionado</h4>
   
  <form name="qryform" id="qryform" method="post" action="mail.php" onsubmit="return(validate());" novalidate="novalidate">
     <div class="form-group">
       <label>Name:</label>
       <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name">
-    </div>
-    <div class="form-group">
-      <label>Email:</label>
-      <input type="email" class="form-control" id="name" placeholder="Enter Email" name="email">
-    </div>
-    
-    <div class="form-group">
-      <label>Phone No.:</label>
-      <input type="text" class="form-control" id="phone" placeholder="Enter Phone no." name="phone">
-    </div>
-	<div class="form-group">
-      <label>Subject:</label>
-      <input type="text" class="form-control" id="name" placeholder="Subject" name="subject">
-    </div>
-	 
-	<div class="form-group">
-      <label>Issues/query:</label>
-      <textarea name="issues" class="form-control" id="iq" placeholder="Enter your Issues/query"></textarea>
-    </div>
-	
-    
+    </div>	
     <button type="submit" class="btn btn-default">Submit</button>
   </form>
   </div>
