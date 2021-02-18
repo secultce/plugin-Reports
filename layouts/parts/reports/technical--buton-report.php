aad0c483789bd91e216253e8bc22f51151a51dbd
joaovitorcavalcante <contato.cavalcantebarbosa@gmail.com>
2021-02-18 11:15:08 -0300

    fixing bug technical button

--- layouts/parts/reports/technical--buton-report.php
+++ layouts/parts/reports/technical--buton-report.php
@@ -2,19 +2,19 @@
 use MapasCulturais\App;
 use MapasCulturais\i;
 
-$route = App::i()->createUrl('opportunity', 'documentQualificationSummary', ['id'=>$entity->id]);
+$route = App::i()->createUrl('technical', 'report', ['id'=>$entity->id]);
 
 ?>
 
 <!--botão de imprimir-->
-<a class="btn btn-default download" ng-click="editbox.open('report-evaluation-documental-options', $event)"
-    rel="noopener noreferrer">Imprimir Resultado Documental</a>
+<a class="btn btn-default download" ng-click="editbox.open('report-evaluation-technical-options', $event)"
+    rel="noopener noreferrer">Imprimir Resultado Técnico</a>
 
 <!-- Formulário -->
-<edit-box id="report-evaluation-documental-options" position="top"
+<edit-box id="report-evaluation-technical-options" position="top"
     title="<?php i::esc_attr_e('Imprimir Resultado')?>"
     cancel-label="Cancelar" close-on-cancel="true">
-    <form class="form-report-evaluation-documental-options"
+    <form class="form-report-evaluation-technical-options"
         action="<?=$route?>" method="POST">
         <label for="publishDate">Data publicação</label>
         <input type="date" name="publishDate" id="publishDate">
