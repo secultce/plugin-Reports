ab7dc8a97c19c294d753ceb188bb591077d0943b
WylderM <wyldermenezes@gmail.com>
2021-03-09 20:40:03 -0300

    relatório concluído

--- Controllers/PagamentoAuxilioEventos.php
+++ Controllers/PagamentoAuxilioEventos.php
@@ -29,34 +29,66 @@ class PagamentoAuxilioEventos extends Report
         $outputReportFile = __DIR__ . '/../temp-files';
         $inputReportFile = __DIR__ . '/../jasper/build/relatorio-pagamentos-auxilio-eventos.jasper';
 
-        // $sqlData = "";
-        // $stmt = $app->em->getConnection()->prepare($sqlData);
-        // $stmt->execute();
-        // $data = $stmt->fetchAll();
-        // $json_array = [];
-        // foreach ($data as $d) {
-        //     $funcao = json_decode($d['funcao']);
-        //     $funcaoString = $funcao[0];
-        //     $json_array[] = [
-        //         'n_inscricao' => $d['num_inscricao'],
-        //         'proponente' => $d['proponente'],
-        //         'funcao' => $funcaoString,
-        //         'municipio' => $d['municipio'],
-        //         'situacao' => $d['situacao'],
-        //         'motivo' => $d['motivo']
-        //     ];
-        // }
-        $json_array[] = [
-            "n_inscricao" => "ON-341311137",
-            "nome" => "ALDJANE LIMA DE OLIVEIRA",
-            "funcao" => "PRODUTOR",
-            "municipio" => "FORTALEZA",
-            "parcela_1" => "AGUARDANDO RETORNO",
-            "motivo_1" => "CONTA CORRENTE INVALIDA",
-            "parcela_2" => "PENDENDTE",
-            "motivo_2" => "PENDENDTE"
-        ];
+        $sqlData = "
+            select distinct 
+                r.number as n_inscricao,
+                r.agents_data::json->'owner'->>'nomeCompleto' nome,
+                rm.value as funcao,
+                r.agents_data::json->'owner'->>'En_Municipio' municipio,
+                sp.valor as valor_parcela,
+                CASE
+                WHEN sp.status = 0 then 'PEDENTE'
+                WHEN sp.status = 1 then 'AGUARDANDO ENVIO'
+                WHEN sp.status = 2 then 'AGUARDANDO RETORNO'
+                WHEN sp.status = 3 then 'PAGO'
+                WHEN sp.status = 4 then 'ERRO'
+                end as status,
+                sp.error as motivo_erro,
+                sp.parcela as parcela	
+            from
+                public.registration as r
+                    left join public.registration_evaluation as re
+                        ON re.registration_id = r.id
+                    INNER join public.registration_meta as rm
+                        ON rm.object_id = r.id
+                    left join public.secultce_payments as sp
+                        on sp.registration_id = r.id
+            where
+                opportunity_id = 2852
+                and r.status = 1
+                and rm.key = 'field_26552'
+                and sp.parcela in (1,2)
+        ";
 
+        $stmt = $app->em->getConnection()->prepare($sqlData);
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        $json_data = [];
+        $json_array = [];
+        foreach ($data as $d) {
+            $funcao = json_decode($d['funcao']);
+            $funcaoString = $funcao[0];
+            $json_data[] = [
+                'n_inscricao' => $d['n_inscricao'],
+                "nome" => $d['nome'],
+                "funcao" => $funcaoString,
+                "municipio" => $d['municipio'],
+                "parcela_1" => $d['status'],
+                "motivo_1" => $d['motivo_erro'],
+                "parcela_2" => $d['status'],
+                "motivo_2" => $d['motivo_erro']
+            ];
+        }
+        $json_array[] = [
+            'n_inscricao' => $json_data[0]['n_inscricao'],
+            "nome" => $json_data[0]['nome'],
+            "funcao" => $funcaoString,
+            "municipio" => $json_data[0]['municipio'],
+            "parcela_1" => $json_data[0]['parcela_1'],
+            "motivo_1" => $json_data[0]['motivo_1'],
+            "parcela_2" => $json_data[1]['parcela_2'],
+            "motivo_2" => $json_data[1]['motivo_2']
+        ];
         $driver = 'json';
         $query = null;
         $params = [
